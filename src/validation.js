2975e754b34d6cca5527b1e7f465f1c06fcc3b02
Portfolio Developer <dev@portfolio.local>
2026-05-25 21:03:18 +0300

    Add password validation

--- src/validation.js
+++ src/validation.js
@@ -7,4 +7,6 @@ module.exports = { validateEmail };
 
 function sanitizeInput(input) {
     return input.replace(/[<>]/g, '');  // защита от XSS
+function validatePassword(password) {
+    return password.length >= 8;
 }
